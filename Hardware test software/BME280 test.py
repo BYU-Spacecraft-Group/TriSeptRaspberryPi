3672f83d969425c1c41b5e7378e248b665532b47
Tyler Nielsen <tylernielsenn@gmail.com>
2022-05-01 13:00:53 -0400

    added altitude sensoring

--- Hardware test software/BME280 test.py
+++ Hardware test software/BME280 test.py
@@ -22,6 +22,7 @@ while True:
     print("\nTemperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
     print("Measure Time: " + str(time.time() - start))
-    #print("Altitude = %0.2f meters" % bme280.altitude)
+
     #time.sleep(2)

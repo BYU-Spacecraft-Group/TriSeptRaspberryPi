f845b1aca5d77235a51cda1d6404a1633defd9c0
Tyler Nielsen <tylernielsenn@gmail.com>
2022-05-01 00:10:48 -0400

    adding timing to bme individual test file

--- Hardware test software/BME280 test.py
+++ Hardware test software/BME280 test.py
@@ -18,8 +18,10 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 #bme280.sea_level_pressure = 1013.25
 
 while True:
+    start = time.time()
     print("\nTemperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Measure Time: " + str(time.time() - start))
     #print("Altitude = %0.2f meters" % bme280.altitude)
-    time.sleep(2)
+    #time.sleep(2)

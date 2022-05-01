9bd2a003405d8e1ce85ffacfec75eb02f1365962
Tyler Nielsen <tylernielsenn@gmail.com>
2022-04-30 23:47:40 -0400

    readded ms5803 code

--- CombinedCode/totalCombined.py
+++ CombinedCode/totalCombined.py
@@ -133,93 +133,93 @@ try:
 		dataIn += f"{mpr.pressure}, "
 
 		#### ms5803 pressure
-		# try:  #this currently only works sometimes -
-		# 		# I'm not sure why. I found a 3rd party library for it 
-		# 		# (with looping example code) but it does about the same
-		# 	bus.write_byte(0x76, 0x1E)
+		try:  #this currently only works sometimes -
+				# I'm not sure why. I found a 3rd party library for it 
+				# (with looping example code) but it does about the same
+			bus.write_byte(0x76, 0x1E)
 
-		# 	time.sleep(0.005)
+			time.sleep(0.005)
 
-		# 	# Read 12 bytes of calibration data
-		# 	# Read pressure sensitivity
-		# 	data = bus.read_i2c_block_data(0x76, 0xA2, 2)
-		# 	C1 = data[0] * 256 + data[1]
+			# Read 12 bytes of calibration data
+			# Read pressure sensitivity
+			data = bus.read_i2c_block_data(0x76, 0xA2, 2)
+			C1 = data[0] * 256 + data[1]
 
-		# 	# Read pressure offset
-		# 	data = bus.read_i2c_block_data(0x76, 0xA4, 2)
-		# 	C2 = data[0] * 256 + data[1]
+			# Read pressure offset
+			data = bus.read_i2c_block_data(0x76, 0xA4, 2)
+			C2 = data[0] * 256 + data[1]
 
-		# 	# Read temperature coefficient of pressure sensitivity
-		# 	data = bus.read_i2c_block_data(0x76, 0xA6, 2)
-		# 	C3 = data[0] * 256 + data[1]
+			# Read temperature coefficient of pressure sensitivity
+			data = bus.read_i2c_block_data(0x76, 0xA6, 2)
+			C3 = data[0] * 256 + data[1]
 
-		# 	# Read temperature coefficient of pressure offset
-		# 	data = bus.read_i2c_block_data(0x76, 0xA8, 2)
-		# 	C4 = data[0] * 256 + data[1]
+			# Read temperature coefficient of pressure offset
+			data = bus.read_i2c_block_data(0x76, 0xA8, 2)
+			C4 = data[0] * 256 + data[1]
 
-		# 	# Read reference temperature
-		# 	data = bus.read_i2c_block_data(0x76, 0xAA, 2)
-		# 	C5 = data[0] * 256 + data[1]
+			# Read reference temperature
+			data = bus.read_i2c_block_data(0x76, 0xAA, 2)
+			C5 = data[0] * 256 + data[1]
 
-		# 	# Read temperature coefficient of the temperature
-		# 	data = bus.read_i2c_block_data(0x76, 0xAC, 2)
-		# 	C6 = data[0] * 256 + data[1]
+			# Read temperature coefficient of the temperature
+			data = bus.read_i2c_block_data(0x76, 0xAC, 2)
+			C6 = data[0] * 256 + data[1]
 
-		# 	# MS5803_01BA address, 0x76(118)
-		# 	#		0x40(64)	Pressure conversion(OSR = 256) command
-		# 	bus.write_byte(0x76, 0x40)
+			# MS5803_01BA address, 0x76(118)
+			#		0x40(64)	Pressure conversion(OSR = 256) command
+			bus.write_byte(0x76, 0x40)
 
-		# 	time.sleep(0.005)
+			time.sleep(0.005)
 
-		# 	# Read digital pressure value
-		# 	# Read data back from 0x00(0), 3 bytes
-		# 	# D1 MSB2, D1 MSB1, D1 LSB
-		# 	value = bus.read_i2c_block_data(0x76, 0x00, 3)
-		# 	D1 = value[0] * 65536 + value[1] * 256 + value[2]
+			# Read digital pressure value
+			# Read data back from 0x00(0), 3 bytes
+			# D1 MSB2, D1 MSB1, D1 LSB
+			value = bus.read_i2c_block_data(0x76, 0x00, 3)
+			D1 = value[0] * 65536 + value[1] * 256 + value[2]
 
-		# 	# MS5803_01BA address, 0x76(118)
-		# 	#		0x50(64)	Temperature conversion(OSR = 256) command
-		# 	bus.write_byte(0x76, 0x50)
+			# MS5803_01BA address, 0x76(118)
+			#		0x50(64)	Temperature conversion(OSR = 256) command
+			bus.write_byte(0x76, 0x50)
 
-		# 	time.sleep(0.005)
+			time.sleep(0.005)
 
-		# 	# Read digital temperature value
-		# 	# Read data back from 0x00(0), 3 bytes
-		# 	# D2 MSB2, D2 MSB1, D2 LSB
-		# 	value = bus.read_i2c_block_data(0x76, 0x00, 3)
-		# 	D2 = value[0] * 65536 + value[1] * 256 + value[2]
+			# Read digital temperature value
+			# Read data back from 0x00(0), 3 bytes
+			# D2 MSB2, D2 MSB1, D2 LSB
+			value = bus.read_i2c_block_data(0x76, 0x00, 3)
+			D2 = value[0] * 65536 + value[1] * 256 + value[2]
 
-		# 	dT = D2 - C5 * 256
-		# 	TEMP = 2000 + dT * C6 / 8388608
-		# 	OFF = C2 * 65536 + (C4 * dT) / 128
-		# 	SENS = C1 * 32768 + (C3 * dT ) / 256
-		# 	T2 = 0
-		# 	OFF2 = 0
-		# 	SENS2 = 0
+			dT = D2 - C5 * 256
+			TEMP = 2000 + dT * C6 / 8388608
+			OFF = C2 * 65536 + (C4 * dT) / 128
+			SENS = C1 * 32768 + (C3 * dT ) / 256
+			T2 = 0
+			OFF2 = 0
+			SENS2 = 0
 
-		# 	if TEMP >= 2000 :
-		# 		T2 = 0
-		# 		OFF2 = 0
-		# 		SENS2 = 0
-		# 		if TEMP > 4500 :
-		# 			SENS2 = SENS2 - ((TEMP - 4500) * (TEMP - 4500)) / 8
-		# 	elif TEMP < 2000 :
-		# 		T2 = (dT * dT) / 2147483648
-		# 		OFF2 = 3 * ((TEMP - 2000) * (TEMP - 2000))
-		# 		SENS2 = 7 * ((TEMP - 2000) * (TEMP - 2000)) / 8
-		# 		if TEMP < -1500 :
-		# 			SENS2 = SENS2 + 2 * ((TEMP + 1500) * (TEMP + 1500))
+			if TEMP >= 2000 :
+				T2 = 0
+				OFF2 = 0
+				SENS2 = 0
+				if TEMP > 4500 :
+					SENS2 = SENS2 - ((TEMP - 4500) * (TEMP - 4500)) / 8
+			elif TEMP < 2000 :
+				T2 = (dT * dT) / 2147483648
+				OFF2 = 3 * ((TEMP - 2000) * (TEMP - 2000))
+				SENS2 = 7 * ((TEMP - 2000) * (TEMP - 2000)) / 8
+				if TEMP < -1500 :
+					SENS2 = SENS2 + 2 * ((TEMP + 1500) * (TEMP + 1500))
 
-		# 	TEMP = TEMP - T2
-		# 	OFF = OFF - OFF2
-		# 	SENS = SENS - SENS2
-		# 	pressure = ((((D1 * SENS) / 2097152) - OFF) / 32768.0) / 100.0
-		# 	cTemp = TEMP / 100.0
-		# 	fTemp = cTemp * 1.8 + 32
+			TEMP = TEMP - T2
+			OFF = OFF - OFF2
+			SENS = SENS - SENS2
+			pressure = ((((D1 * SENS) / 2097152) - OFF) / 32768.0) / 100.0
+			cTemp = TEMP / 100.0
+			fTemp = cTemp * 1.8 + 32
 
-		# 	dataIn += f"{pressure}, {cTemp}, {fTemp}, "
-		# except OSError:
-		dataIn += "-, -, -, "
+			dataIn += f"{pressure}, {cTemp}, {fTemp}, "
+		except OSError:
+			dataIn += "-, -, -, "
 		
 		#### RTC
 		t = rtc.datetime

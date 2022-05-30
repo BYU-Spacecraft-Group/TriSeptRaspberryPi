7cd52dfb32178c6f7694eca6f554efb0fc2a1a4c
RySaw <ryansawyer98@icloud.com>
2022-05-30 13:11:57 -0600

    added new file for testing BME

---
+++ Hardware test software/BME280newAdvanced.py
@@ -0,0 +1,26 @@
+import board
+import adafruit_bme280.advanced as adafruit_bme280
+import time
+
+i2c = board.I2C()
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+bme280.sea_level_pressure = 1013.25
+
+bme280.overscan_pressure = OVERSCAN_
+
+start = time.time()
+
+temperature = bme280.temperature
+relative_humidity = bme280.relative_humidity
+pressure = bme280.pressure
+altitude = bme280.altitude
+
+print()
+print("Sense Time: " + str(time.time() - start))
+print("Temp: " + str(temperature))
+print("Humidity: " + str(relative_humidity))
+print("Pressure: " + str(pressure))
+print("Altitude: " + str(altitude))
+print()
+

258a31ce235e3c7e66c08db15073d0289a3c3e38
Tyler Nielsen <tylernielsenn@gmail.com>
2022-06-07 23:05:47 -0400

    rough draft off i2c devices

--- SortedCode/i2cDevices.py
+++ SortedCode/i2cDevices.py
@@ -4,6 +4,8 @@ Devices in this file: bme280, imu, mprls, rtc
 
 # added to be more robust if any error is raised it will try to execute the code again
 # but I don't think any error will happen
+TIMING = True
+
 while True:
     try: 
         import time
@@ -18,7 +20,7 @@ while True:
         it = 0
         dataPipe = []
 
-        dataPipe.append("SystemTime")
+        dataPipe.append("SystemTime, Temp, Hum, Pres, AccelX, AccelY, AccelZ, GyroX, GyroY, GyroZ, MPRPressure, RTC, \n")
 
         #bme280
         import board #also: gps, imu, mprls pressure, rtc
@@ -60,4 +62,61 @@ while True:
         continue
     break
 
+while True: # another retry scheme 
+    try:
+        while True:
+            startCollect = time.time()
+            print(it)
+            #collecting data
+            dataIn = "" #string to save as a line
 
+            #### SystemTime
+            startSysTime = time.time()
+            dataIn += f"{time.time()}, "
+            print("System Time Log: " + str(time.time() - startSysTime)) 
+
+            #### bme280 ----- Currently takes around the longest time - ~0.03s
+            startBME = time.time()
+            dataIn += f"{str(bme280.temperature)}, {bme280.relative_humidity}, {bme280.pressure}, "
+            print("BME Time: " + str(time.time() - startBME))
+
+            #### imu 
+            startIMU = time.time()
+            dataIn += "%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, " % (ism.acceleration + ism.gyro)
+            print("IMU Time: " + str(time.time() - startIMU))
+
+            #### mprls pressure
+            startMPR = time.time()
+            dataIn += f"{mpr.pressure}, "
+            print("MPR Time: " + str(time.time() - startMPR))
+
+            #### RTC
+            startRTC = time.time()
+            t = rtc.datetime
+
+            dataIn += f"{t.tm_hour}:{t.tm_min}:{t.tm_sec}, "
+            print("RTC Time: " + str(time.time() - startRTC))
+
+            dataPipe.append(str(dataIn) + "\n")
+
+            print("Data collect time: " + str(time.time() - startCollect))
+
+            #saving data
+            it += 1
+            if(it % STORING_GAP == 0):
+                start = time.time() # for timing
+                file = open(f"Results{ID}.csv", 'a')
+                file.writelines(dataPipe)
+                file.flush()
+                os.fsync(file.fileno())
+                file.close()
+                #clear stored values
+                dataPipe.clear()
+                    
+                print(f"{it}: Write time: " + str(time.time() - start))
+                
+    except OSError:
+        break
+    except:
+        continue
+    # without break because it should never break 
